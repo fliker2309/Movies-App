338d87b520d323024f0ac5177bb3afd78a3d8347
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2022-05-26 18:29:31 +0300

    build main screen

---
+++ app/src/main/java/com/example/moviecompose/screens/DetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.moviecompose.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.moviecompose.MainViewModel
+
+@Composable
+fun DetailsScreen(navController: NavController, viewModel: MainViewModel){
+
+}
--- app/src/main/java/com/example/moviecompose/screens/MainScreen.kt
+++ app/src/main/java/com/example/moviecompose/screens/MainScreen.kt
@@ -1,44 +1,101 @@
 package com.example.moviecompose.screens
 
-import android.util.Log
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
 import com.example.moviecompose.MainViewModel
 import com.example.moviecompose.data.models.Movies
+import com.example.moviecompose.navigation.Screens
+import com.example.moviecompose.utils.Constants
 
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel) {
     val allMovies = viewModel.allMovies.observeAsState(listOf()).value
-    allMovies.forEach { Log.d("checkData", " Id ${it.id}, name: ${it.name}") }
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        LazyColumn{
+        LazyColumn(
+            modifier = Modifier
+                .padding(20.dp)
+        ) {
             items(allMovies.take(10)) { item ->
-                MovieItem(item = item)
+                MovieItem(item = item, navController = navController)
             }
         }
     }
 }
 
 @Composable
-fun MovieItem(item: Movies) {
-    Row(
+fun MovieItem(item: Movies, navController: NavHostController) {
+    Card(
+        elevation = 4.dp,
         modifier = Modifier
-            .fillMaxWidth()
-
+            .padding(top = 8.dp)
+            .clickable {
+                navController.navigate(Screens.Details.route + "/${item.id}")
+            }
     ) {
-        Text(text = item.id.toString())
-        Text(text = item.name)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+        ) {
+            Image(
+                painter = rememberImagePainter(item.image.medium),
+                contentDescription = null,
+                modifier = Modifier.size(120.dp)
+
+            )
+            Column {
+                Text(
+                    text = item.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+
+                )
+                Row(modifier = Modifier) {
+                    Text(
+                        text = "Rating: ",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Text(text = item.rating.average.toString())
+                }
+                Row(modifier = Modifier) {
+                    Text(
+                        text = "Genre: ",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    item.genres.take(2).forEach { Text(text = "$it ") }
+                }
+                Row(modifier = Modifier) {
+                    Text(
+                        text = "Premiere: ",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Text(text = item.premiered)
+                }
+            }
+        }
     }
 }
